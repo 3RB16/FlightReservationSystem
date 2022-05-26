57010a2d34a6d39fcf9369ef3e6d96eae1040bbc
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-05-26 20:27:39 +0200

    Add files via upload

---
+++ packages/DatePicker.HTMLHelper.1.2.2.1/content/App_Start/DatePickerHelperBundleConfig.cs.pp
@@ -0,0 +1,19 @@
+﻿using System.Web.Optimization;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.DatePickerHelperBundleConfig), "RegisterBundles")]
+
+namespace $rootnamespace$.App_Start
+{
+	public class DatePickerHelperBundleConfig
+	{
+		public static void RegisterBundles()
+		{
+            BundleTable.Bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
+            "~/Scripts/bootstrap-datepicker.js",
+            "~/Scripts/locales/bootstrap-datepicker.*"));
+
+            BundleTable.Bundles.Add(new StyleBundle("~/Content/datepicker").Include(
+            "~/Content/bootstrap-datepicker.css"));
+		}
+	}
+}
